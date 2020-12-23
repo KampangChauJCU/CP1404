4537982140a6b160e6b6e7fcc3c3329b593365ff
Jinpeng.zhou <jinpeng.zhou@my.jcu.edu.au>
2020-12-23 17:36:48 +0800

    Car & used_cars function fixing

--- Practical6/used_cars.py
+++ Practical6/used_cars.py
@@ -3,7 +3,7 @@ from Practical6.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car("My car", 180)
+    my_car = Car("Lexus LFA", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
